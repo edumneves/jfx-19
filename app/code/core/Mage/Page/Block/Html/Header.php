166f844f8e137c5a2326f95208f69659f01d031c
edumneves <edumneves@gmail.com>
2014-11-08 08:31:27 -0200

    Acertando varnish da mensagem de boas vindas.

--- app/code/core/Mage/Page/Block/Html/Header.php
+++ app/code/core/Mage/Page/Block/Html/Header.php
@@ -89,11 +89,12 @@ class Mage_Page_Block_Html_Header extends Mage_Core_Block_Template
     public function getWelcome()
     {
         if (empty($this->_data['welcome'])) {
-            if (Mage::isInstalled() && Mage::getSingleton('customer/session')->isLoggedIn()) {
-                $this->_data['welcome'] = $this->__('Welcome, %s!', $this->escapeHtml(Mage::getSingleton('customer/session')->getCustomer()->getName()));
-            } else {
-                $this->_data['welcome'] = Mage::getStoreConfig('design/header/welcome');
-            }
+//            if (Mage::isInstalled() && Mage::getSingleton('customer/session')->isLoggedIn()) {
+//                $this->_data['welcome'] = $this->__('Welcome, %s!', $this->escapeHtml(Mage::getSingleton('customer/session')->getCustomer()->getName()));
+//            } else {
+//                $this->_data['welcome'] = Mage::getStoreConfig('design/header/welcome');
+//            }
+            $this->_data['welcome'] = Mage::getStoreConfig('design/header/welcome');
         }
 
         return $this->_data['welcome'];
